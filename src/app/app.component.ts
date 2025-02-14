361ab652550ba5a2acf48f1d257610f458819ef6
Edgardo <ecofre@gmail.com>
2025-02-13 23:18:16 -0300

    Added resources javascript and css and form to upload a file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-upload-demo';
+    title = 'angular-upload-demo';
+    selectedFile: File | null = null;
+
+    onFileSelected(event: Event): void {
+      const input = event.target as HTMLInputElement;
+      if (input.files && input.files.length > 0) {
+        this.selectedFile = input.files[0];
+      }
+    }
+
+    uploadFile(): void {
+      if (!this.selectedFile) {
+          return;
+      }
+      const formData = new FormData();
+      formData.append('file', this.selectedFile);
+      
+    }  
+
 }
+
+
